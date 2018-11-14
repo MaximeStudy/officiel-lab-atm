8231ec48fcf068523a4df1177af36625dd441f4f
Maxime Study <maxime.study@gmail.com>
2018-11-14 17:33:50 -0500

    Probleme 4. Oublie d instanciation

---
+++ src/test/java/ca/ulaval/glo4002/atm/large/WithdrawLargeTest.java
@@ -0,0 +1,20 @@
+package ca.ulaval.glo4002.atm.large;
+
+import javax.ws.rs.core.Response.Status;
+
+import org.junit.Test;
+
+import ca.ulaval.glo4002.atm.application.banking.WithdrawalRequest;
+import ca.ulaval.glo4002.atm.rest.RestTestBase;
+
+public class WithdrawLargeTest extends RestTestBase {
+    @Test
+    public void canWithdrawMoney() {
+        WithdrawalRequest request = new WithdrawalRequest(100);
+
+        givenBaseRequest().body(request)
+                .when()
+                .put("/accounts/{accountNumber}/withdraw", buildPathParams("accountNumber", ACCOUNT_NUMBER_WITH_MONEY))
+                .then().statusCode(Status.OK.getStatusCode());
+    }
+}
---
+++ src/test/java/ca/ulaval/glo4002/atm/suites/LargeITestSuite.java
@@ -0,0 +1,33 @@
+package ca.ulaval.glo4002.atm.suites;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import ca.ulaval.glo4002.atm.AtmServer;
+import ca.ulaval.glo4002.atm.contexts.LargeTestContext;
+import ca.ulaval.glo4002.atm.large.WithdrawLargeTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({ WithdrawLargeTest.class })
+public class LargeITestSuite {
+
+    public static final int TEST_SERVER_PORT = 9292;
+
+    private static AtmServer atmServer;
+
+    @BeforeClass
+    public static void setUpClass() {
+        atmServer = new AtmServer();
+        atmServer.start(TEST_SERVER_PORT, new LargeTestContext());
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        if (atmServer != null) {
+            atmServer.stop();
+        }
+    }
+}
